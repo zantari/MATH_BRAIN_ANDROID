b1142b628adab9d3031940798a3bd6e271112d0b
zantari <ewaind8@gmail.com>
2025-11-25 21:13:25 +0100

    added new layout

--- app/src/main/java/com/example/android_math_brain/LevelActivity.kt
+++ app/src/main/java/com/example/android_math_brain/LevelActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
 
 class LevelActivity : AppCompatActivity() {
     private var levelId = -1
@@ -121,9 +122,15 @@ class LevelActivity : AppCompatActivity() {
                 loadQuestion()
             } else {
                 Toast.makeText(this, "WYGRALES! ", Toast.LENGTH_SHORT).show()
-
-                finish()
+                Intent(this, EndLevelActivity::class.java).also {
+                    startActivity(it)
+                }
             }
+
+
+
+
+
         }
         else{
             Toast.makeText(this, "zla odpowiedz ", Toast.LENGTH_LONG).show()
