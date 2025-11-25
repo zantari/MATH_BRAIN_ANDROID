ff91917a2de3ade0f696cb5a418bb8ab0e8279d5
lolix1321 git config --global user.email lolix.1321@gmail.com <twoj@email.com>
2025-11-25 01:41:49 +0100

    ADVENTURE ZROBIONY MYSLE (tylko dopracowac wizualnie)

--- app/src/main/java/com/example/android_math_brain/AdventureActivity.kt
+++ app/src/main/java/com/example/android_math_brain/AdventureActivity.kt
@@ -1,23 +1,24 @@
 package com.example.android_math_brain
 
-import android.app.Activity
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.Rect
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ScrollView
-import android.widget.TextView
 
 class AdventureActivity : AppCompatActivity() {
     private lateinit var gameData: GameData // nie usuwaj
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        gameData = GameData.getInstance(this) //nie usuwaj
-
-
+        gameData = GameData.getInstance(this) // nie usuwaj
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,23 +34,94 @@ class AdventureActivity : AppCompatActivity() {
             scrollView.fullScroll(ScrollView.FOCUS_DOWN)
         }
 
-
-
-
-
-
-
-        //mozesz wyjebac pod spodem
-
-        val buttonPowrot = findViewById<ImageButton>(R.id.goBack);
-
-
-
+        val buttonPowrot = findViewById<ImageButton>(R.id.goBack)
         buttonPowrot.setOnClickListener {
-
             finish()
         }
 
+        // === NOWA FUNKCJONALNOŚĆ ===
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val button10 = findViewById<Button>(R.id.button10)
+        val button11 = findViewById<Button>(R.id.button11)
+        val button12 = findViewById<Button>(R.id.button12)
+        val button13 = findViewById<Button>(R.id.button13)
+        val button14 = findViewById<Button>(R.id.button14)
+        val button15 = findViewById<Button>(R.id.button15)
+        val button16 = findViewById<Button>(R.id.button16)
+        val button17 = findViewById<Button>(R.id.button17)
+        val button18 = findViewById<Button>(R.id.button18)
+        val button19 = findViewById<Button>(R.id.button19)
+        val button20 = findViewById<Button>(R.id.button20)
+        val button21 = findViewById<Button>(R.id.button21)
+        val button22 = findViewById<Button>(R.id.button22)
+        val button23 = findViewById<Button>(R.id.button23)
+        val button24 = findViewById<Button>(R.id.button24)
+        val button25 = findViewById<Button>(R.id.button25)
+        val button26 = findViewById<Button>(R.id.button26)
+        val button27 = findViewById<Button>(R.id.button27)
+        val button28 = findViewById<Button>(R.id.button28)
+        val button29 = findViewById<Button>(R.id.button29)
+        val button30 = findViewById<Button>(R.id.button30)
+        val button31 = findViewById<Button>(R.id.button31)
+        val button32 = findViewById<Button>(R.id.button32)
+        val button33 = findViewById<Button>(R.id.button33)
+        val button34 = findViewById<Button>(R.id.button34)
+        val button35 = findViewById<Button>(R.id.button35)
+        val button36 = findViewById<Button>(R.id.button36)
+        val button37 = findViewById<Button>(R.id.button37)
+        val button38 = findViewById<Button>(R.id.button38)
+        val button39 = findViewById<Button>(R.id.button39)
+        val button40 = findViewById<Button>(R.id.button40)
 
-    }
-}
+
+        val chapterName = findViewById<TextView>(R.id.chapterName)
+        val topLayout = findViewById<LinearLayout>(R.id.topLayout)
+        val goBack = findViewById<ImageButton>(R.id.goBack)
+
+
+
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val scrollBounds = Rect()
+            scrollView.getHitRect(scrollBounds)
+
+            // Sprawdzenie, czy przycisk 11 jest widoczny
+            if (button11.getLocalVisibleRect(scrollBounds) || button14.getLocalVisibleRect(scrollBounds)) {
+                chapterName.text = "SUBTRACTION"
+                chapterName.setTextColor(Color.parseColor("#793679"))
+                topLayout.setBackgroundColor(Color.parseColor("#A249A3"))
+                goBack.background.setColorFilter(Color.parseColor("#A249A3"), PorterDuff.Mode.SRC_IN)
+
+            } else if (button21.getLocalVisibleRect(scrollBounds) || button24.getLocalVisibleRect(scrollBounds)) {
+                chapterName.text = "MULTIPLICATION"
+                chapterName.setTextColor(Color.parseColor("#A30D14"))
+                topLayout.setBackgroundColor(Color.parseColor("#EB1C24"))
+                goBack.background.setColorFilter(Color.parseColor("#EB1C24"), PorterDuff.Mode.SRC_IN)
+
+            } else if (button31.getLocalVisibleRect(scrollBounds) || button35.getLocalVisibleRect(scrollBounds)) {
+                chapterName.text = "DIVISION"
+                chapterName.setTextColor(Color.parseColor("#C5BB00"))
+                topLayout.setBackgroundColor(Color.parseColor("#FDF000"))
+                goBack.background.setColorFilter(Color.parseColor("#FDF000"), PorterDuff.Mode.SRC_IN)
+
+            } else {
+                chapterName.text = "ADDITION"
+                chapterName.setTextColor(Color.parseColor("#22B045"))
+                topLayout.setBackgroundColor(Color.parseColor("#B4E41D"))
+                goBack.background.setColorFilter(
+                    Color.parseColor("#B4E41D"),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }
+            // === KONIEC NOWEJ FUNKCJONALNOŚCI ===
+        }}}
+
+
+
